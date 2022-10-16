2faa01927acd25a810474fc8da72aedeb55fdd8f
jiyunjie <jyj12345@163.com>
2022-10-16 17:31:54 +0800

    学习

---
+++ src/components/Study.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>{{ count }}</div>
+  <div>{{ state.count }}</div>
+  <button @click="update">点击</button>
+</template>
+
+<script lang="ts" setup>
+import { ref, reactive } from "vue";
+
+const count = ref<number>(0);
+const state = reactive({
+  count: 1,
+});
+
+function update(): void {
+  count.value++;
+  state.count++;
+}
+</script>
